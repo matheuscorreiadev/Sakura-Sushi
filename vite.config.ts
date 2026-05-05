27de6688f3875b51774788c639655149f06a4949
Matheus Correia <matheuscorreia1910@gmail.com>
2026-05-05 10:37:56 -0300

    configurei o vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Sakura-Sushi/',
+
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    // hmr: {
+    //   overlay: false,
+    // },
   },
-  base: '/Sakura-Sushi/',
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
